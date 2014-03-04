b10216d25efedb4089ff02b143a7d52b10e1d2ad
waltergms <walter.gms@gmail.com>
2014-03-04 00:29:14 -0300

    04-03-2014
    Engine, nova estrutura, projeto teste funcionando

---
+++ CORE/code/ProjetoTeste/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+	bool grounded, interact; //bool for checking if player is grounded so they can jump, and bool for interact, so that player can only interact when in range of thing to interact with
+	public Transform jumpCheck, interactCheck; //transform variable for the end points of the linecasts
+
+	RaycastHit2D interacted; //a variable type that stores a collider that was hit during linecast
+
+	public float speed = 6.0f;
+
+	public float speedVertical = 0;
+	public float speedHorizontal = 0;
+
+	float jumpTime, jumpDelay = .3f;
+	bool jumped;
+
+	Animator anim;
+
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		Movement(); //call the function every frame
+		RaycastStuff(); //call the function every frame
+	}
+
+	void RaycastStuff()
+	{
+		//Just a debug visual representation of the Linecast, can only see this in scene view! Doesn't actually do anything!
+		Debug.DrawLine(transform.position, jumpCheck.position, Color.magenta);
+		Debug.DrawLine(transform.position, interactCheck.position, Color.magenta);
+
+		//we assign the bool 'ground' with a linecast, that returns true or false when the end of line 'jumpCheck' touches the ground
+		grounded = Physics2D.Linecast(transform.position, jumpCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+
+		//Using linecast which takes (start point, end point, layermask) so we can make it only detect objects with specified layers
+		//its wrapped in an if statement, so that while the tip of the Linecast (interactCheck.position) is touching an object with layer 'Guard', the code inside executes
+		if(Physics2D.Linecast(transform.position, interactCheck.position, 1 << LayerMask.NameToLayer("Guard")))
+		{
+			//we store the collider object the Linecast hit so that we can do something with that specific object, ie. the guard
+			//each time the linecast touches a new object with layer "guard", it updates 'interacted' with that specific object instance
+			interacted = Physics2D.Linecast(transform.position, interactCheck.position, 1 << LayerMask.NameToLayer("Guard")); 
+			interact = true; //since the linecase is touching the guard and we are in range, we can now interact!
+		}
+		else
+		{
+			interact = false; //if the linecast is not touching a guard, we cannot interact
+		}
+
+		Physics2D.IgnoreLayerCollision(8, 10); //if we want certain layers to ignore each others collision, we use this! the number is the layer number in the layers list
+	}
+
+
+	void Movement() //function that stores all the movement
+	{
+        speedVertical = 0;
+        speedHorizontal = 0;
+
+		if(Input.GetAxisRaw("Horizontal") > 0)
+		{
+			speedHorizontal = Mathf.Abs(Input.GetAxisRaw ("Horizontal")); 
+			transform.Translate(Vector3.right * speed * Time.deltaTime); 
+			transform.eulerAngles = new Vector2(0, 0); //this sets the rotation of the gameobject
+		}
+		
+		if(Input.GetAxisRaw("Horizontal") < 0)
+		{
+			speedHorizontal = Mathf.Abs(Input.GetAxisRaw ("Horizontal"));
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 180); //this sets the rotation of the gameobject
+		}
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            speedVertical = Mathf.Abs(Input.GetAxisRaw("Vertical"));
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0, 270);
+            
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            speedVertical = Mathf.Abs(Input.GetAxisRaw("Vertical"));
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0, 90);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            anim.SetTrigger("push");
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetTrigger("stopPush");
+        }
+
+        if (speedHorizontal > 0)
+        {
+            anim.SetFloat("speed", speedHorizontal);
+        }
+        else
+        {
+            anim.SetFloat("speed", speedVertical);
+        }
+	}
+}
